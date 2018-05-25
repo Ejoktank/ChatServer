c68928ed73b4e689fbb3f9a2a0edf8e58bc3fa38
SoNiceElijah <Superil2q@yandex.ru>
2018-05-25 19:33:39 +0300

    Простейшая логика на register запрос

--- main.js
+++ main.js
@@ -1,12 +1,24 @@
 var express = require("express");
+var bodyParser = require("body-parser");
 
 var app = express();
+var jsonParser = bodyParser.json();
 
 app.get("/", function(request, response){
      
-    response.send("<h2>Привет Express!</h2>");
+  response.send("<h1>Корень</h1>");
+});
+app.post("/register", jsonParser, function(request, response) {
+   if(!request.body) return response.sendStatus(400);
+   console.log(request.body);
 
-  });
+   var res = {
+    IsRegistred: true,
+    ErrorReason: null
+  };
+
+  response.send(JSON.stringify(res));
+});
 
 app.listen(1234);
 console.log("listen on port: 1234");
